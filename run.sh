f0e1cd257010807d1bfb9a4e1f319e406080c235
Iosif Spartalis <iosifspart@gmail.com>
2018-03-13 19:29:27 +0200

    changes in docker-compose.yml

--- run.sh
+++ run.sh
@@ -3,9 +3,12 @@
 mkdir -p output
 chmod 777 output
 
+echo 'setup and mipmap_db i2b2_db_harmonized'
 docker-compose up -d mipmap_db i2b2_db_harmonized #i2b2_db 
 
+echo 'setup and mipmap_db i2b2_db_harmonized'
 docker-compose up wait_dbs
 #docker-compose run --rm i2b2_setup
+echo 'remove i2b2_setup_harmonized'
 docker-compose run --rm i2b2_setup_harmonized
 docker-compose up mipmap_etl
