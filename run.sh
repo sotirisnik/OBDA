6be749010c1f561f4b641874f6a89ae39696a1d4
Iosif Spartalis <iosifspart@gmail.com>
2018-03-13 18:49:20 +0200

    commented a line in run.sh script

--- run.sh
+++ run.sh
@@ -6,6 +6,6 @@ chmod 777 output
 docker-compose up -d mipmap_db i2b2_db_harmonized #i2b2_db 
 
 docker-compose up wait_dbs
-docker-compose run --rm i2b2_setup
+#docker-compose run --rm i2b2_setup
 docker-compose run --rm i2b2_setup_harmonized
 docker-compose up mipmap_etl
