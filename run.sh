2cdeec06047364b8e0207102e588b835e8102e8e
Iosif Spartalis <iosifspart@gmail.com>
2018-03-14 16:13:28 +0200

    another test

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ chmod 777 output
 echo 'setup and mipmap_db i2b2_db_harmonized'
 docker-compose up -d mipmap_db i2b2_db_harmonized #i2b2_db 
 
-echo 'setup and mipmap_db i2b2_db_harmonized'
+echo 'setup wait_dbs'
 docker-compose up wait_dbs
 #docker-compose run --rm i2b2_setup
 echo 'remove i2b2_setup_harmonized'
